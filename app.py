3f36f0df8851c927acac2cc67ee80875f87972ab
Loc Le <loc.tan.le@gmail.com>
2018-03-07 14:11:19 -0800

    applicantdataentry

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
+import requests
+import tweepy
+
 
 app = Flask(__name__)
-
 mongo = PyMongo(app)
+
+tweet_data = {
+    "tweet_source": [],
+    "tweet_text": [],
+    "tweet_date": [],
+    "tweet_vader_score": [],
+    "tweet_neg_score": [],
+    "tweet_pos_score": [],
+    "tweet_neu_score": []
+}
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/login')
-def login():
-    
+@app.route('/applicantdataentry')
+def applicant():
+    return render_template('applicant_Data_Entry.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
