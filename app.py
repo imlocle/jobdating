c41a86dba37ca66733fbc4c0d5b9f10233b86330
Loc Le <loc.tan.le@gmail.com>
2018-03-07 10:55:55 -0800

    adding basic flask set up

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ import tweepy
 import json
 import pandas as pd
 from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
 import matplotlib.pyplot as plt
 
 
+app = Flask(__name__)
+
+mongo = PyMongo(app)
+
+@app.route("/login", methods=["POST"])
+def register():
+   return render_template("/")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
